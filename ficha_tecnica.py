6942c10856fd6c46487ab0f540f9b3f882779489
gustavo-moretto <gustavomorlin92@gmail.com>
2023-07-08 11:28:52 -0300

    update from colab

--- ficha_tecnica.py
+++ ficha_tecnica.py
@@ -6,6 +6,7 @@ from pydicom import *
 
 # Selecting DICOM Data
 data = dcmread('C:\\Users\\User\\Documents\\repos\\ficha_tecnica_blumenau\\testing_files\\RP.729634.REPLAN.dcm')
+
 ##################################################################################
 # Extracting Basic Infos
 # Patient Name
@@ -68,15 +69,58 @@ for beam in range(n_beams):
     beam_number = beam  # using this to avoid mistakes on numering
     key         = str(beam_number + 1)
     beam_name   = data.BeamSequence[beam].BeamName
+    # Checking if phase is correct for each beam
+    check_phase = input(f'{beam_name} - {beam_presc[str(beam+1)][0]} is it correct? [yes/no]: ')
+    if check_phase == 'yes':
+      pass
+    else:
+      beam_presc[str(beam+1)] = input('Please, write the correct phase: ')
     machine     = machine
     energy      = data.BeamSequence[beam].ControlPointSequence[0].NominalBeamEnergy
-    x1          = abs(float(data.BeamSequence[beam].ControlPointSequence[0].BeamLimitingDevicePositionSequence[0].LeafJawPositions[0]) / 10)
-    x2          = float(data.BeamSequence[beam].ControlPointSequence[0].BeamLimitingDevicePositionSequence[0].LeafJawPositions[1]) / 10
-    y1          = abs(float(data.BeamSequence[beam].ControlPointSequence[0].BeamLimitingDevicePositionSequence[1].LeafJawPositions[0]) / 10)
-    y2          = float(data.BeamSequence[beam].ControlPointSequence[0].BeamLimitingDevicePositionSequence[1].LeafJawPositions[1]) / 10
-    gantry      = data.BeamSequence[beam].ControlPointSequence[0].GantryAngle
-    collimator  = data.BeamSequence[beam].ControlPointSequence[0].BeamLimitingDeviceAngle
-    couch       = data.BeamSequence[beam].ControlPointSequence[0].PatientSupportAngle
+    # Treatment technique
+    # We will extract gantry, collimator, etc, inside the beam technique, to lead with differences when we have modulated techniques
+    if data.BeamSequence[beam].BeamType == 'STATIC':
+      x1          = abs(float(data.BeamSequence[beam].ControlPointSequence[0].BeamLimitingDevicePositionSequence[0].LeafJawPositions[0]) / 10)
+      x2          = float(data.BeamSequence[beam].ControlPointSequence[0].BeamLimitingDevicePositionSequence[0].LeafJawPositions[1]) / 10
+      y1          = abs(float(data.BeamSequence[beam].ControlPointSequence[0].BeamLimitingDevicePositionSequence[1].LeafJawPositions[0]) / 10)
+      y2          = float(data.BeamSequence[beam].ControlPointSequence[0].BeamLimitingDevicePositionSequence[1].LeafJawPositions[1]) / 10
+      gantry      = data.BeamSequence[beam].ControlPointSequence[0].GantryAngle
+      collimator  = data.BeamSequence[beam].ControlPointSequence[0].BeamLimitingDeviceAngle
+      couch       = data.BeamSequence[beam].ControlPointSequence[0].PatientSupportAngle
+      technique   = '3D'
+    elif data.BeamSequence[beam].BeamType == 'DYNAMIC':
+      x1          = '---'
+      x2          = '---'
+      y1          = '---'
+      y2          = '---'
+      if data.BeamSequence[beam].ControlPointSequence[0].GantryRotationDirection == 'NONE':
+        gantry      = str(data.BeamSequence[beam].ControlPointSequence[0].GantryAngle)
+        collimator  = data.BeamSequence[beam].ControlPointSequence[0].BeamLimitingDeviceAngle
+        couch       = data.BeamSequence[beam].ControlPointSequence[0].PatientSupportAngle
+        technique   = 'IMRT'
+      else:
+        gantry      = str(data.BeamSequence[beam].ControlPointSequence[0].GantryAngle) + f'({str(data.BeamSequence[beam].ControlPointSequence[0].GantryRotationDirection)})'
+        collimator  = data.BeamSequence[beam].ControlPointSequence[0].BeamLimitingDeviceAngle
+        couch       = data.BeamSequence[beam].ControlPointSequence[0].PatientSupportAngle
+        technique   = 'VMAT'
+    else:
+      technique = '---'
+    # Wedges
+    if data.BeamSequence[beam].NumberOfWedges == '1':
+      # Extracting wedge angle, type and orientation
+      wedge    = str(data.BeamSequence[beam].WedgeSequence[0].WedgeAngle) + str(data.BeamSequence[beam].WedgeSequence[0].WedgeType) + str(data.BeamSequence[beam].WedgeSequence[0].WedgeOrientation)
+      # Some cases we have a percentage of the field with wedge and other without (Elekta Motorized Wedge)
+      # So, we will use a condition to extract correct MU values in this cases
+      if data.BeamSequence[beam].WedgeSequence[0].WedgeType == 'MOTORIZED':
+        wedge_mu = round(float(data.BeamSequence[beam].ControlPointSequence[1].CumulativeMetersetWeight) * beam_mu_data[str(beam+1)][0],1)
+        beam_mu_data[str(beam+1)] = str(wedge_mu) + '/' + str(beam_mu_data[str(beam+1)][0])
+    else:
+      wedge = '---'
+    # Bolus
+    if data.BeamSequence[beam].NumberOfBoli == '1':
+      bolus = data.BeamSequence[beam].ReferencedBolusSequence[0].BolusDescription
+    else:
+      bolus = '---'
     # Some cases the Source to Surface Distance needs to add manually
     ssd = ''
     try:
@@ -87,7 +131,43 @@ for beam in range(n_beams):
       ssd = str(f'100/{ssd}')
     beams_data[key] = [machine, energy, beam_name,
                        x1, x2, y1, y2,
-                       gantry, collimator, couch, ssd]
+                       gantry, collimator, couch, wedge, bolus,
+                       technique, ssd]
+  elif data.BeamSequence[beam].TreatmentDeliveryType == 'SETUP':
+    beam_number = data.BeamSequence[beam].BeamNumber
+    beam_number = beam  # using this to avoid mistakes on numering
+    key         = str(beam_number + 1)
+    beam_name   = data.BeamSequence[beam].BeamName
+    # Checking if phase is correct for each beam
+    check_phase = input(f'{beam_name} - {beam_presc[str(beam+1)][0]} is it correct? [yes/no]: ')
+    if check_phase == 'yes':
+      pass
+    else:
+      beam_presc[str(beam+1)] = input('Please, write the correct phase: ')
+    machine     = machine
+    energy      = data.BeamSequence[beam].ControlPointSequence[0].NominalBeamEnergy
+    x1          = '---'
+    x2          = '---'
+    y1          = '---'
+    y2          = '---'
+    gantry      = data.BeamSequence[beam].ControlPointSequence[0].GantryAngle
+    collimator  = data.BeamSequence[beam].ControlPointSequence[0].BeamLimitingDeviceAngle
+    couch       = data.BeamSequence[beam].ControlPointSequence[0].PatientSupportAngle
+    technique   = 'SETUP'
+    wedge       = '---'
+    bolus       = '---'
+    # Some cases the Source to Surface Distance needs to add manually
+    ssd = ''
+    try:
+      ssd = round(float(data.BeamSequence[beam].ControlPointSequence[0].SourceToSurfaceDistance) / 10, 1)
+      ssd = str(f'100/{ssd}')
+    except AttributeError:
+      ssd = input(f"Input SSD for Beam {beam_name}: ")
+      ssd = str(f'100/{ssd}')
+    beams_data[key] = [machine, energy, beam_name,
+                       x1, x2, y1, y2,
+                       gantry, collimator, couch, wedge, bolus,
+                       technique, ssd]
   else:
     pass
 
@@ -97,12 +177,21 @@ if __name__ == '__main__':
   beam_mu_data   = pd.DataFrame.from_dict(beam_mu_data)
   beam_presc     = pd.DataFrame.from_dict(beam_presc)
   plan_data      = pd.concat([beams_data, beam_mu_data])
-  plan_data      = pd.concat([plan_data, beam_presc]).set_index([['Machine', 'Energy', 'Beam',
+  plan_data      = pd.concat([plan_data, beam_presc]).set_index([['Equipamento', 'Energia', 'Local',
                                                                   'X1', 'X2', 'Y1', 'Y2',
-                                                                  'Gantry', 'Collimator', 'Couch', 'SSD',
-                                                                  'MU', 'Phase']])
-  final_data     = plan_data.reindex(['Machine', 'Energy', 'Beam',
-                                      'X1', 'X2', 'Y1', 'Y2', 'SSD',
-                                      'MU', 'Gantry', 'Collimator', 'Couch', 'Phase']).T.dropna()
+                                                                  'Ang. Gantry', 'Ang. Colimador', 'Ang. Mesa', 'Filtro', 'Bólus',
+                                                                  'Técnica', 'DFTu/DFPe', 'MU', 'Fase']])
+  final_data     = plan_data.reindex(['Técnica', 'Equipamento', 'Energia', 'Local',
+                                      'X1', 'X2', 'Y1', 'Y2', 'DFTu/DFPe',
+                                      'MU', 'Ang. Gantry', 'Ang. Colimador', 'Ang. Mesa', 'Filtro', 'Bólus', 'Fase']).T.dropna()
+  print('---' * 30)
+  print(plan_name)
   print(name, birth, id)
-  print(final_data.T)
+  final_data = final_data.T
+  birth_save = birth.replace('/','')
+  file_name = f'{plan_name}_{name}_{birth_save}_{id}'
+  final_data.to_excel(f'{file_name}.xlsx')
+
+#####################################################################
+# Opening save sheet
+open(f'{file_name}.xlsx', 'r')
